e79da1924d8caaf606e4a9789287dc53d339f28e
asuXie <michal.galuszka@student.urk.edu.pl>
2023-11-08 22:56:47 +0100

    SECOND COMMIT

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import random
 from datetime import datetime
 from currency_converter import CurrencyConverter
-from tkinter import Tk
 c = CurrencyConverter()
 
 dtNow = datetime.now()
@@ -10,11 +9,18 @@ dtNowString = dtNow.strftime("%d/%m/%Y %H:%M:%S")
 class Currency:
     pass
 class User:
-    def __init__(self, id, name, surname, cardNumber):
+    def __init__(self, id):
+        
+        name = str(input("Podaj imię: ")) 
+        surname = str(input("Podaj nazwisko: "))
+        
+
+
+
         self.id = id
-        self.name = name
-        self.surname = surname
-        self.balance = {
+        self._name = name
+        self._surname = surname
+        self._balance = {
             "USD":0,
             "EUR":0,
             "JPY":0,
@@ -22,9 +28,9 @@ class User:
             "AUD":0,
             "PLN":0
                         }
-        self.cardNumber = cardNumber 
-        self.currienciesNum = 0
-        self.currency = {
+        self._cardNumber = random.randrange(100000, 999999)
+        self._currienciesNum = 0
+        self._currency = {
             "USD":False,
             "EUR":False,
             "JPY":False,
@@ -33,6 +39,7 @@ class User:
             "PLN":False
                         }
         self.history = []
+        print("Twój numer karty", self._cardNumber)
     
     def typeAssign(self, amount, variety, currency):
         result = (variety, amount, currency, dtNowString)
@@ -48,14 +55,14 @@ class User:
         currency.upper()
         withdrawal = float(input("Kwota do wypłacenia: "))
         withdrawal = round(withdrawal, 2)
-        for key in self.currency:
+        for key in self._currency:
             if key == currency:
-                if withdrawal <= self.balance[currency]:
-                    self.balance[currency] -= withdrawal
+                if withdrawal <= self._balance[currency]:
+                    self._balance[currency] -= withdrawal
                     print("Operacja zakończona pomyślnie")   
                     self.typeAssign(variety, withdrawal, currency)
-                    if self.balance[currency] == 0:
-                        self.currency[currency] = False
+                    if self._balance[currency] == 0:
+                        self._currency[currency] = False
                         
 
     def deposit(self): #wpłata
@@ -64,60 +71,65 @@ class User:
         currency.upper()
         deposit = float(input("Kwota do wpłacenia: "))
         deposit = round(deposit, 2)
-        for key in self.currency:
+        for key in self._currency:
             if key == currency:
-                self.balance[currency] += deposit
-                self.currency[currency] = True
+                self._balance[currency] += deposit
+                self._currency[currency] = True
                 print("Operacja zakończona pomyślnie")
                 self.typeAssign(variety, deposit, currency)      
                    
 
     def userInfo(self): #info o użytkowniku
-        print("Imię: ", self.name) 
-        print("Nazwisko: ", self.surname)
-        print("Numer karty: ", self.cardNumber)
+        print("Imię: ", self._name) 
+        print("Nazwisko: ", self._surname)
+        print("Numer karty: ", self._cardNumber)
         print(" ")
         print("Waluty: ")
-        for keys in self.balance:
-            print(keys, "Kwota: ", self.balance[keys])
+        for keys in self._balance:
+            print(keys, "Kwota: ", self._balance[keys])
    
     def currenciesAvailable(self, current):
         currencyAvailability = False
-        if self.currency[current] == True:
+        if self._currency[current] == True:
             currencyAvailability = True
         return currencyAvailability
-
     
-    
-
-
-def currencyTransfer():
-    type = "Currency Transfer"
-    fromCurrency = str(input("Podaj walutę do przewalutowania: "))
-    toCurrency = str(input("Podaj walutę docelową: "))
-    amount = float(input("Podaj kwotę: "))
-    if account[id].currenciesAvailable(fromCurrency) == True:
-        tempMoney = c.convert(amount, fromCurrency, toCurrency)
+    def validation(self, cardNumber, surname):
+        if self._cardNumber == cardNumber and self._surname == surname:
+            return True
+        else:
+            return False
         
-        tempMoney = round(tempMoney, 2)
-        account[id].balance[fromCurrency] -= amount
-        account[id].balance[toCurrency] += tempMoney
-        print("Operacja zakończona pomyślnie")
+    def currencyTransfer(self):
     
-    else:
-        print("Operacja nieudana")
+        fromCurrency = str(input("Podaj walutę do przewalutowania: "))
+        toCurrency = str(input("Podaj walutę docelową: "))
+        amount = float(input("Podaj kwotę: "))
+        if self.currenciesAvailable(fromCurrency) == True:
+            tempMoney = c.convert(amount, fromCurrency, toCurrency)
+            
+            tempMoney = round(tempMoney, 2)
+            self._balance[fromCurrency] -= amount
+            self._balance[toCurrency] += tempMoney
+            print("Operacja zakończona pomyślnie")
+        
+        else:
+            print("Operacja nieudana")
+    
+    
+
+
+
 
 
 accountsNum = int(input("Podaj liczbę użytkowników: "))
 account = []
 
 for id in range(0, accountsNum):
-    name = str(input("Podaj imię: ")) 
-    surname = str(input("Podaj nazwisko: "))
-    cardNumber = random.randrange(100000, 999999)  ##Losowanie liczby - nr karty 
-    print("Twój numer karty", cardNumber)
+    
+    account.append(User(id)) ##Powoływanie obiektu User
 
-    account.append(User(id, name, surname, cardNumber)) ##Powoływanie obiektu User
+
 term = 1
 while term != 0:
     print("1. Zaloguj się")
@@ -131,14 +143,14 @@ while term != 0:
     
 
             for id in range(0, accountsNum):
-                if account[id].cardNumber == cardNumber and account[id].surname == surname:
+                if account[id].validation(cardNumber, surname) == True:
                    termInside = 1
                    while termInside != 0:
                         print("1. Wyświetl dane konta")    
                         print("2. Wpłać") 
                         print("3. Wypłać") 
                         print("4. Historia transakcji")
-                        print("4. Transfer walut") 
+                        print("5. Transfer walut") 
                         print("0. Wyloguj")
                         termInside = int(input("Wybierz: "))
 
@@ -158,7 +170,7 @@ while term != 0:
                                 account[id].typeShow()
                             
                             case 5:
-                                currencyTransfer()
+                                account[id].currencyTransfer()
             
             else:
                 print("Brak użytkownika: ", surname)
